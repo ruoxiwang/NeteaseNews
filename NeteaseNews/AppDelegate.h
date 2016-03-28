dd958bdf1e771e9040c122a4c638e4977b8dfed8
ruoxiwang <wangrunliang@hotmail.com>
2016-03-28 10:09:57 +0800

    Initial Commit

---
+++ NeteaseNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NeteaseNews
+//
+//  Created by 王若曦 on 16/3/28.
+//  Copyright © 2016年 wrx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NeteaseNews/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NeteaseNews
+//
+//  Created by 王若曦 on 16/3/28.
+//  Copyright © 2016年 wrx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
